4d92886ebcc9763792fb668d0a86c61f288be600
SmallPeenwow <bowdencamryn@gmail.com>
2022-12-29 09:38:28 +0200

    Beginning Of Learning

---
+++ helloworld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "I like pizza!" << std::endl;
+    std::cout << "It's really good" << std::endl;
+    std::cout << "Nice trick to use" << '\n';
+
+    return 0;
+}
