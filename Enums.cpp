533d8d778ae1d2ecc534ed54dd7123f3b244bf35
SmallPeenwow <bowdencamryn@gmail.com>
2023-01-13 10:33:15 +0200

    55. Enums

---
+++ Enums.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+enum Day {
+    sunday = 0,
+    monday = 1,
+    tuesday = 2,
+    wednesday = 3,
+    thursday = 4,
+    friday = 5,
+    saturday = 6,
+};
+
+enum Flavor {
+    vanilla,
+    chocolate,
+    strawberry,
+    mint
+};
+
+enum Color {
+    red, 
+    orange,
+    yellow,
+    green,
+    blue,
+    purple,
+};
+
+enum Planet {
+    mercury = 4880,
+    venus = 12104,
+    earth = 12756,
+    mars = 6794,
+    jupiter = 142984,
+    saturn = 108728,
+    uranus = 51118,
+    neptune = 49532,
+    pluto = 2320,
+};
+
+int main()
+{
+    // enums = a user-defined data type that consists
+    //         of paired named-integer constants.
+    //         GREAT if you have a set of potential options
+
+    Day today = sunday;
+
+    switch (today)
+    {
+        case sunday:
+            std::cout << "It is Sunday!" << std::endl;
+            break;
+        case monday:
+            std::cout << "It is Monday!" << std::endl;
+            break;
+        case tuesday:
+            std::cout << "It is Tuesday!" << std::endl;
+            break;
+        case wednesday:
+            std::cout << "It is Wednesday!" << std::endl;
+            break;
+        case thursday:
+            std::cout << "It is Thursday!" << std::endl;
+            break;
+        case friday:
+            std::cout << "It is Friday!" << std::endl;
+            break;
+        case saturday:
+            std::cout << "It is Saturday!" << std::endl;
+            break;
+    }
+
+    return 0;
+}
